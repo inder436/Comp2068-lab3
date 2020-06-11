4634c3b61aee904c74eb6c7a36a0b5782ba2c8fe
Inderdeep Singh Chawla <200412907@student.georgianc.on.ca>
2020-06-11 16:53:55 -0400

    Add project files.

---
+++ Lab3/routes/Brother.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+
+    if (req.query)
+        var name1 = "Brother";
+
+    res.render('Brother', { name: name1 });
+});
+
+module.exports = router;
---
+++ Lab3/routes/Inder.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    if (req.query)
+        var name = "Inder";
+
+    res.render('Inder', { name: name });
+});
+
+module.exports = router;
